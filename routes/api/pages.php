52102e416b8650e6d935c59b2476232772b92de2
freed-coder <freed@arabianpay.net>
2025-07-17 13:36:59 +0300

    Add pages

--- routes/api/pages.php
+++ routes/api/pages.php
@@ -2,7 +2,7 @@
 
 
 use App\Http\Controllers\PagesController;
-Route::prefix('v2')->middleware(['auth:sanctum', 'sanctum.auth.json'])->group(function () {
+Route::prefix('v2/pages')->middleware(['auth:sanctum', 'sanctum.auth.json'])->group(function () {
     Route::get('/terms-conditions-client', [PagesController::class, 'getTermsConditions']);
     Route::get('/replacement-policy', [PagesController::class, 'getReplacementPolicy']);
 });
