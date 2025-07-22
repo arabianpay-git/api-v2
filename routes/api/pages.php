ca56e7785349564283b3b41f12b80ad1a43e9d86
freed-coder <freed@arabianpay.net>
2025-07-22 10:23:17 +0300

    Update and add new Apis - add privacyPolicy

--- routes/api/pages.php
+++ routes/api/pages.php
@@ -5,4 +5,6 @@ use App\Http\Controllers\PagesController;
 Route::prefix('v2/pages')->group(function () {
     Route::get('/terms-conditions-client', [PagesController::class, 'getTermsConditions']);
     Route::get('/replacement-policy', [PagesController::class, 'getReplacementPolicy']);
+    Route::get('/privacy-policy', [PagesController::class, 'getReplacementPolicy']);
+    Route::get('/simah-policy', [PagesController::class, 'getReplacementPolicy']);
 });
