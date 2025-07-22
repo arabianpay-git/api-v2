049f5aaccfec3b6dd2af79fd1ae34d5d3fcf321a
freed-coder <freed@arabianpay.net>
2025-07-22 10:50:15 +0300

    Update and add new Apis - remove sanctum from addresses

--- routes/api/addresses.php
+++ routes/api/addresses.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\AddressesController;
 
-Route::prefix('v2/addresses/')->middleware(['auth:sanctum', 'sanctum.auth.json'])->group(function () {
+Route::prefix('v2/addresses/')->middleware(['sanctum.auth.json'])->group(function () {
     Route::get('get', [AddressesController::class, 'getAddresses']);
     Route::post('store', [AddressesController::class, 'store']);
     Route::post('update/{address_id}', [AddressesController::class, 'update']);
