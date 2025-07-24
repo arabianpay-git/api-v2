6e4ed94f969461495d8bf7dc767a6b3b9fc294ac
freed-coder <freed@arabianpay.net>
2025-07-24 12:07:05 +0300

    update get-common-question

--- routes/api/common.php
+++ routes/api/common.php
@@ -10,8 +10,9 @@ Route::prefix('v2')->middleware(['auth:sanctum', 'sanctum.auth.json'])->group(fu
     Route::get('/common/get-cities-by-state/{state_id}', [CommonController::class, 'getCities']);
     Route::get('/common/get-config-system-app', [CommonController::class, 'getConfigSystemApp']);
     Route::get('/common/get-social-media', [CommonController::class, 'getSocialMedia']);
-    Route::get('/common/get-common-question', [CommonController::class, 'getCommonQuestion']);
     Route::post('/common/change-language', [CommonController::class, 'changeLanguage']);
     Route::post('/common/ads-statistics', [CommonController::class, 'adsStatistics']);
     
 });
+
+Route::get('v2/common/get-common-question', [CommonController::class, 'getCommonQuestion']);
