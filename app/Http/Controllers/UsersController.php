b64f19771c94289c6c9a3c9e2b6ea91c5fab65bd
freed-coder <freed@arabianpay.net>
2025-07-30 15:56:37 +0300

    update add slides

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -78,7 +78,7 @@ class UsersController extends Controller
         // Load sliders, banners, top store, etc.
         $dashboardSlider = AdsSlider::take(10)->get()->map(function ($item) {
             return [
-                'image' => $item->image?? 'https://api.arabianpay.com/uploads/sliders/default_cover.png',
+                'image' => $item->image?'https://core.arabianpay.com'.$item->image:'https://api.arabianpay.com/uploads/sliders/default_cover.png',
                 'image_id' => (string) $item->id,
                 'target' => [
                     'type' => 'null',
