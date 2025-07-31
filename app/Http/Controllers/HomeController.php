3d04fe515a23b00280e5b0244b9c60a4df5bb3b8
freed-coder <freed@arabianpay.net>
2025-07-31 12:57:39 +0300

    update home products

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -345,6 +345,7 @@ class HomeController extends Controller
                 'current_stock'
             ])
             ->with(['brand:id,name'])
+            ->where('name','!=',null)
             ->where('thumbnail','!=',null)
             ->where('published', 'published')
             ->orderByDesc('number_of_sales')
@@ -396,6 +397,7 @@ class HomeController extends Controller
                 'current_stock'
             ])
             ->with(['brand:id,name'])
+            ->where('name','!=',null)
             ->where('published', 'published')
             ->where('featured', 1)
             ->where('thumbnail','!=',null)
