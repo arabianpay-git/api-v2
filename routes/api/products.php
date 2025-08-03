2b274f397409c52c350b500d82e95a9a8fdd0bc8
freed-coder <freed@arabianpay.net>
2025-08-03 10:37:02 +0300

    add related products

--- routes/api/products.php
+++ routes/api/products.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\ProductsController;
 Route::prefix('v2')->middleware(['auth:sanctum', 'sanctum.auth.json'])->group(function () {
     Route::get('/products/all', [ProductsController::class, 'index']);
     Route::get('/product-details/{id}', [ProductsController::class, 'getProductDetails']);
+    Route::get('/products/related/{id}', [ProductsController::class, 'getProductRelated']);
     Route::get('/products/filter', [ProductsController::class, 'getProductFilters']);
     Route::post('/products/check-variation', [ProductsController::class, 'checkVariation']);
 });
