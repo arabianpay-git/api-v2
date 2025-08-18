ee61bc95191eb082bb687e5ef012de3edc636db6
freed-coder <freed@arabianpay.net>
2025-08-18 16:45:30 +0300

    update payments list

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -568,6 +568,7 @@ class OrdersController extends Controller
             $transaction = Transaction::where('uuid', $referenceId)
             ->where('user_id', $request->user()->id)->first();
 
+            return $transaction;
             if (!empty($transaction)) {
                 $order = Order::query()
                 ->where('id', $transaction->order_id ?? 0)
