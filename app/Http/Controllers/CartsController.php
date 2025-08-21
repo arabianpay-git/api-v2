15d653b4aedec4850011b4bbcdbb88859370dae1
freed-coder <freed@arabianpay.net>
2025-08-21 18:45:21 +0300

    add otp sms

--- app/Http/Controllers/CartsController.php
+++ app/Http/Controllers/CartsController.php
@@ -194,7 +194,7 @@ class CartsController extends Controller
             $cart = Cart::with('items')->where('user_id', $user->id)->latest()->first();
 
             if (!$cart) {
-               return $this->returnData(json_encode([]), 'Cart not found.');
+               return $this->returnData(null, 'Cart not found.');
             }
 
             // Calculate total discount
@@ -235,7 +235,7 @@ class CartsController extends Controller
 
             return $this->returnData($data, 'Cart fetched successfully');
         } catch (\Exception $e) {
-           return $this->returnData(json_encode([]), 'Cart not found.');
+           return $this->returnData(null, 'Cart not found.');
         }
     }
     
